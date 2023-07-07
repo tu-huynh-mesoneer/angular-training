8d119bda931cd769516e785c27a18cf94e8f935d
taahutu1 <tu.huynh@swisscom.com>
2023-07-07 09:26:24 +0700

    add ngrx training

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { HttpClientModule } from '@angular/common/http';
-
-import { appReducer } from './store';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './effects';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsModule } from './posts/posts.module';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot({})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    PostsModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- ngrx/src/app/store/user/user.states.ts
+++
@@ -1,9 +0,0 @@
-export interface IUserLoginState {
-  loading: boolean;
-  success: boolean;
-  fail: boolean;
-  userName: string;
-}
-export interface IUserState {
-  login: IUserLoginState;
-}
